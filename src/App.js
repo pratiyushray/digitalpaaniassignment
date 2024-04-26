0d8ec88c59e5a75b7dda6bdbc5eafd210a3acbd6
Pratiyush Kumar Ray <pkray0021@gmail.com>
2024-04-26 23:35:29 +0530

    initialCommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Temperature from "../src/Components/Temprature";
+import Highlights from "../src/Components/Highlights";
 
 function App() {
+  const [city, setCity] = useState("New Delhi");
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=0352f07402fa4706afb95609242504&q=${city}&aqi=no
+    `;
+
+    fetch(apiUrl)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Could not get data");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setWeatherData(data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, [city]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-black h-screen flex justify-center  items-start">
+      <div className="w-1/5 h-1/3 mt-40">
+        {weatherData && (
+          <Temperature
+            setCity={setCity}
+            stats={{
+              temp: weatherData.current.temp_c,
+              condition: weatherData.current.condition.text,
+              isDay: weatherData.current.is_day,
+              location: weatherData.location.name,
+              time: weatherData.location.localtime,
+            }}
+          />
+        )}
+      </div>
+      <div className="w-1/3 h-1/3 mt-40 p-10 grid grid-cols-2 gap-6">
+        <h1 className="text-slate-200 text-2xl col-span-2">
+          Today's Highlights
+        </h1>
+        {weatherData && (
+          <>
+            <Highlights
+              stats={{
+                title: "Wind Status",
+                value: weatherData.current.wind_mph,
+                unit: "mph",
+                direction: weatherData.current.wind_dir,
+              }}
+            />
+            <Highlights
+              stats={{
+                title: "Humidity",
+                value: weatherData.current.humidity,
+                unit: "%",
+              }}
+            />
+            <Highlights
+              stats={{
+                title: "Visibility",
+                value: weatherData.current.vis_miles,
+                unit: "miles",
+              }}
+            />
+            <Highlights
+              stats={{
+                title: "Air Pressure",
+                value: weatherData.current.pressure_mb,
+                unit: "mb",
+              }}
+            />
+          </>
+        )}
+      </div>
     </div>
   );
 }
